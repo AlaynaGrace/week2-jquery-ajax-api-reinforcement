056499a3460e513e3a2a6fb99370e309e6720b78
Alayna Buysse <alaynagrace@comcast.net>
2017-04-19 09:01:10 -0500

    added css

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function onReady(){
     success: function(response){
       $('h1').text(response.Title);
       $('.poster').append("<img src='"+ response.Poster +"'>");
+      $('.poster').effect('bounce',{times: 2}, 'slow');
       var actors = "<p>Starring: "+ response.Actors + "</p>";
       var description = "<p>" + response.Plot + "</p>";
       var rateAndRun = "<p> Rating: " + response.Rated + " Runtime: " + response.Runtime + "</p>";
